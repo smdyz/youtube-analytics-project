2741efc29dd5a123d5c2480c77fde81964128812
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-15 00:41:59 +0300

    strange errors

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,8 @@
+import json
+import requests
+import os
+from googleapiclient.discovery import build
+import isodate
 
 
 class Channel:
@@ -5,8 +10,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key: str = os.getenv("AIzaSyBjD6--COMCnKq1ApAkfdppxWC96_2ly_I")
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+    # def print_info(self) -> None:
+    #     """Выводит в консоль информацию о канале."""
+    #     channel = self.youtube.channels().list(id=self.channel_id).execute()
+    #     print(channel)
