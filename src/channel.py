727c61beb922051c08ae83b17c555d283844755d
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-15 00:49:52 +0300

    error found

--- src/channel.py
+++ src/channel.py
@@ -12,9 +12,10 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.api_key: str = os.getenv("AIzaSyBjD6--COMCnKq1ApAkfdppxWC96_2ly_I")
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        # self.youtube = build('youtube', 'v3', developerKey=self.api_key)  тут непонятная ошибка
 
-    # def print_info(self) -> None:
-    #     """Выводит в консоль информацию о канале."""
-    #     channel = self.youtube.channels().list(id=self.channel_id).execute()
-    #     print(channel)
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        # channel = self.youtube.channels().list(id=self.channel_id).execute()  данная часть не срабатывает из-за ошибки выше
+        # print(channel)
+        pass
