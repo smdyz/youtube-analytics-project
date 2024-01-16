1cf3f6a4855909107e41560bc5b21dfd5e97a5c1
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-16 22:56:12 +0300

    second hw almost done

--- src/channel.py
+++ src/channel.py
@@ -10,11 +10,34 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.api_key: str = os.getenv("API_KEY")
-        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet, statistics').execute()
+
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.sub_quantity = int(self.channel["items"][0]["statistics"]["subscriberCount"])
+        self.video_count = int(self.channel["items"][0]["statistics"]["videoCount"])
+        self.channel_views = int(self.channel["items"][0]["statistics"]["viewCount"])
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        pass
+
+    def to_json(self, path):
+        info = {'channel_id': self.__channel_id, 'title': self.title,
+                'description': self.description, 'url': self.url, 'sub_quantity': self.sub_quantity,
+                'video_count': self.video_count, 'channel_views': self.channel_views}
+        # with open(path, 'r') as f:
+        #     data = f.read()
+        #     if data != '':
+
+        with open(path, 'a') as file:
+            file.write(json.dumps(info, indent=2, ensure_ascii=False))
+            file.write('\n\n')
