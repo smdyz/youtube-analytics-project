d8f24bd0f6b74d56131241304b8ae664891a1049
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-01-16 21:45:04 +0300

    first hw done

--- src/channel.py
+++ src/channel.py
@@ -11,11 +11,10 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
-        self.api_key: str = os.getenv("AIzaSyBjD6--COMCnKq1ApAkfdppxWC96_2ly_I")
-        # self.youtube = build('youtube', 'v3', developerKey=self.api_key)  тут непонятная ошибка
+        self.api_key: str = os.getenv("API_KEY")
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        # channel = self.youtube.channels().list(id=self.channel_id).execute()  данная часть не срабатывает из-за ошибки выше
-        # print(channel)
-        pass
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet, statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
